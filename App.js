b62a230c0ce40952c7375ff63c3a4a78c67d44f1
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-12-12 16:46:06 -0800

    add Array, Flatlist, Views, etc

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals =>[...currentGoals, { key: Math.random().toString()}]);
+  };
   return (
-
-
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Course Goals"
-          style={styles.input} />
-        <Button title="ADD" />
+          placeholder="Course Goals" 
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
-
-      </View>
-
-    </View>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+      />
+    </View>   
   );
 }
 // now using stylesheet object styling
@@ -34,5 +47,12 @@ const styles = StyleSheet.create({
     borderWidth: 2, 
     padding: 10, 
     width: '80%' 
+  },
+  listItem: {
+    marginVertical: 4,
+    padding: 20,
+    backgroundColor: 'gold', 
+    borderColor: 'black', 
+    borderWidth: 1
   }
 });
