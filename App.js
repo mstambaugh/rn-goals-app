75517c466c70f5a64eaf0d4ff3dfdd84cef99a13
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-12-13 17:00:59 -0800

    split components from app.js

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button, View, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
   
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals =>[...currentGoals, { key: Math.random().toString()}]);
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { key: Math.random().toString(), value: goalTitle}
+    ]);
   };
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goals" 
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}/>
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList 
         data={courseGoals} 
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item}</Text>
-          </View>
-        )}
+        renderItem={itemData => <GoalItem title={itemData.item.value}/>}
       />
     </View>   
   );
@@ -36,23 +27,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center'
-  },
-  input: {
-    borderColor: 'black', 
-    borderWidth: 2, 
-    padding: 10, 
-    width: '80%' 
-  },
-  listItem: {
-    marginVertical: 4,
-    padding: 20,
-    backgroundColor: 'gold', 
-    borderColor: 'black', 
-    borderWidth: 1
   }
+  
 });
