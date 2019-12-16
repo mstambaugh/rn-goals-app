130718362d96ca98bae5dac13e974f36d99283cf
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-12-15 22:40:26 -0800

    add a lot, modal view, clear text method, cancel button, styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, Button, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Button, View, FlatList } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -7,25 +7,34 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
   
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals, 
       { id: Math.random().toString(), value: goalTitle}
     ]);
+    setIsAddMode(false);
   };
   const removeGoalHandler = goalId => {
     setCourseGoals(currentGoals => {
       return currentGoals.filter((goal) => goal.id !== goalId);
-
     });
-  }
+  };
 
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
 
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput 
+        visible={isAddMode} 
+        onAddGoal={addGoalHandler} 
+        onCancel={cancelGoalAdditionHandler} 
+      />
       <FlatList 
         keyExtractor={(item, index) => item.id}
         data={courseGoals} 
@@ -38,5 +47,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
+  }
 });
